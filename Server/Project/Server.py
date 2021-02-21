9366ccd7752f3a94f9665c684f8316230e641263
EladShriki <elad7575@gmail.com>
2021-02-21 20:17:32 +0200

    Make flask run the react server

--- Server/Project/Server.py
+++ Server/Project/Server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 class Server:
@@ -9,7 +9,11 @@ class Server:
         self._funcs = dict()
         self._methods = dict()
 
-        self._app = Flask("Server")
+        self._app = Flask("Server", static_url_path="/", template_folder="../../Web/build")
+
+        self._app.register_error_handler(404, lambda a: render_template("index.html"))
+
+        self._app.add_url_rule("/", view_func=lambda: render_template("index.html"), methods=["GET"])
 
     def add_route(self, route, func, method):
         self._app.add_url_rule(route, view_func=func, methods=[method])
