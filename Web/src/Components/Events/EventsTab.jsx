1be86a163e40513050911c5243882b19eecf6e09
EladShriki <elad7575@gmail.com>
2021-04-03 14:54:59 +0300

    Add drill down to bar chart

--- Web/src/Components/Events/EventsTab.jsx
+++ Web/src/Components/Events/EventsTab.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
 import CustomPieChart from './PieChart';
 import {
@@ -46,10 +46,50 @@ const useStyles = makeStyles(() => ({
  */
 function EventsTab({ events }) {
     const classes = useStyles();
+    const [dateFilter, setDateFilter] = useState({
+        year: '\\d+',
+        month: '\\d+',
+        day: '\\d+',
+    });
+    const [dateFactor, setDateFactor] = useState('year');
+
+    const filteredEvents = useMemo(() => {
+        let re = new RegExp(
+            `${dateFilter.month}/${dateFilter.day}/${dateFilter.year}`,
+            'g',
+        );
+
+        return events.filter(({ timeStamp }) =>
+            timeStamp.toLocaleDateString('en-us').match(re),
+        );
+    }, [events, dateFilter]);
+
+    const lastMonthEvents = events.filter((event) => {
+        const currentTime = new Date();
+        const lastMonth = new Date();
+        lastMonth.setMonth(lastMonth.getMonth() - 1);
+        return (
+            event.timeStamp <= currentTime &&
+            event.timeStamp >= lastMonth &&
+            event
+        );
+    });
+
+    const lastDayEvents = events.filter((event) => {
+        const currentTime = new Date();
+        const lastDay = new Date();
+        lastDay.setDate(lastDay.getDate() - 1);
+
+        return (
+            event.timeStamp >= lastDay &&
+            event.timeStamp <= currentTime &&
+            event
+        );
+    });
 
     const groupByAttribute = (attribute) =>
         Object.values(
-            events.reduce((a, event) => {
+            filteredEvents.reduce((a, event) => {
                 if (!a[event[attribute]])
                     a[event[attribute]] = { name: event[attribute], value: 1 };
                 else a[event[attribute]].value++;
@@ -57,18 +97,35 @@ function EventsTab({ events }) {
                 return a;
             }, {}),
         );
+
     const groupByDate = Object.values(
-        events.reduce((a, { timeStamp }) => {
-            const date = `${
-                timeStamp.getUTCMonth() + 1
-            }/${timeStamp.getFullYear()}`;
+        filteredEvents.reduce((a, { timeStamp }) => {
+            let fullDate;
+            let date;
+            switch (dateFactor) {
+                case 'year':
+                    date = timeStamp.getFullYear();
+                    fullDate = date;
+                    break;
+                case 'month':
+                    date = timeStamp.getMonth() + 1;
+                    fullDate = `${date}/${timeStamp.getFullYear()}`;
+                    break;
+                case 'day':
+                    date = timeStamp.getDate();
+                    fullDate = `${date}/${
+                        timeStamp.getMonth() + 1
+                    }/${timeStamp.getFullYear()}`;
+                    break;
+                default:
+                    break;
+            }
             if (!a[date])
                 a[date] = Object.assign(
                     {},
                     {
-                        date: timeStamp.toLocaleDateString('en-us', {
-                            month: 'long',
-                        }),
+                        date,
+                        fullDate,
                         events: 1,
                     },
                 );
@@ -78,6 +135,39 @@ function EventsTab({ events }) {
         }, {}),
     );
 
+    const onRightClick = (event) => {
+        event.preventDefault();
+
+        if (dateFactor === 'year') return;
+
+        setDateFactor((prev) => {
+            if (prev === 'day') return 'month';
+            if (prev === 'month') return 'year';
+            return prev;
+        });
+        setDateFilter((prev) => {
+            const newState = { ...prev };
+            const newDateFactor = dateFactor === 'day' ? 'month' : 'year';
+            newState[newDateFactor] = '\\d+';
+            return newState;
+        });
+    };
+
+    const onGraphClick = (data) => {
+        if (dateFactor === 'day') return;
+
+        setDateFilter((prev) => {
+            const newState = { ...prev };
+            newState[dateFactor] = data.date;
+            return newState;
+        });
+        setDateFactor((prev) => {
+            if (prev === 'year') return 'month';
+            if (prev === 'month') return 'day';
+            return prev;
+        });
+    };
+
     return (
         <div className={classes.eventsDiv}>
             <div className={classes.headDiv}>
@@ -85,7 +175,10 @@ function EventsTab({ events }) {
                     Number of events: {events.length}
                 </Typography>
                 <Typography color="textPrimary" variant="h5">
-                    Number of idk
+                    Number of events in last month: {lastMonthEvents.length}
+                </Typography>
+                <Typography color="textPrimary" variant="h5">
+                    Number of events in last 24 hours: {lastDayEvents.length}
                 </Typography>
             </div>
             <div className={classes.pieChartsDiv}>
@@ -100,20 +193,17 @@ function EventsTab({ events }) {
                     dataKey="value"
                 />
             </div>
-            <div className={classes.barChartDiv}>
+            <div className={classes.barChartDiv} onContextMenu={onRightClick}>
                 <ResponsiveContainer>
-                    <BarChart data={groupByDate}>
+                    <BarChart data={groupByDate} barSize={80}>
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
+                        <XAxis dataKey="fullDate" />
                         <YAxis />
-                        <Tooltip />
+                        <Tooltip cursor={false} />
                         <Bar
-                            maxBarSize={80}
                             dataKey="events"
                             fill="#8884d8"
-                            onClick={(data, index) =>
-                                console.log('Data: ', data, ' Index: ', index)
-                            }
+                            onClick={onGraphClick}
                         />
                     </BarChart>
                 </ResponsiveContainer>
