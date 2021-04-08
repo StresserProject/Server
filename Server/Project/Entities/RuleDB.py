38099e43fba6ad0221a6c182e7088cd37e07f9f6
EladShriki <elad7575@gmail.com>
2021-04-08 23:43:31 +0300

    Change the ruleData max length to 150

--- Server/Project/Entities/RuleDB.py
+++ Server/Project/Entities/RuleDB.py
@@ -5,4 +5,4 @@ from mongoengine import StringField
 class RuleDB(Document):
     ruleName = StringField(max_length=50, required=True)
     ruleType = StringField(max_length=50, required=True)
-    ruleData = StringField(max_length=50, required=True)
+    ruleData = StringField(max_length=150, required=True)
