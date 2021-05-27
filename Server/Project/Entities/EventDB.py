7a64e7ec75b759982d0aa1eaf087c704bd0be080
EladShriki <elad7575@gmail.com>
2021-05-27 21:12:52 +0300

    Change the eventData max length to 150

--- Server/Project/Entities/EventDB.py
+++ Server/Project/Entities/EventDB.py
@@ -6,7 +6,7 @@ from mongoengine import DateTimeField
 class EventDB(Document):
     eventName = StringField(max_length=50, required=True)
     eventType = StringField(max_length=50, required=True)
-    eventData = StringField(max_length=50, required=True)
+    eventData = StringField(max_length=150, required=True)
     hostname = StringField(max_length=50, required=True)
     IPAddress = StringField(max_length=50, required=True)
     timeStamp = DateTimeField(required=True)
